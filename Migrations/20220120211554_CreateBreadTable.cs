78c8b2e58fe41b9f5278b4afd9bee4ecb6667e92
Kelsey Brown <kbrown55347@gmail.com>
2022-01-20 15:16:41 -0600

    added Pets table to db

---
+++ Migrations/20220120211554_CreateBreadTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreateBreadTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    checkedInAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    PetBreed = table.Column<int>(type: "integer", nullable: false),
+                    PetColor = table.Column<int>(type: "integer", nullable: false),
+                    petOwnerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Pets_PetOwners_petOwnerId",
+                        column: x => x.petOwnerId,
+                        principalTable: "PetOwners",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_petOwnerId",
+                table: "Pets",
+                column: "petOwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pets");
+        }
+    }
+}
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,52 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+  public enum PetBreedType
+  {
+    Shepherd, //0
+    Poodle, //1
+    Beagle,
+    Bulldog,
+    Terrier,
+    Boxer,
+    Labrador,
+    Retriever
+  }
+  public enum PetColorType
+  {
+    Black, //0
+    White, //1
+    Golden,
+    Tricolor,
+    Spotted
+  }
+  public class Pet
+  {
+    // id is special, EF knows it a primary key and serial!
+    public int id { get; set; }
+
+    [Required]
+    public string name { get; set; }
+
+    public DateTime? checkedInAt { get; set; }
+
+    // We need this `JsonConverter` attribute
+    // to convert our `type` string into an Enum
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public PetBreedType PetBreed { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public PetColorType PetColor { get; set; }
+
+    // This is the Id of the pet owner who owns the pet
+    // In a moment, we'll see how .NET can use this field to 
+    // join our tables together for us
+    [ForeignKey("PetOwners")]
+    public int petOwnerId { get; set; }
+
+    // While petOwnerId is an integer with the pet owner's ID,
+    // this field is an actual pet owner object. 
+    public PetOwner ownedBy { get; set; }
+
+  }
 }
